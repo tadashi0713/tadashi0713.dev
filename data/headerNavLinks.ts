8bcbd8d8d6bddc5fa0761eb49b631010db8ed8a8
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-09-28 04:07:08 +0900

    Add nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,8 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/writings', title: 'Writings' },
+  { href: '/slides', title: 'Slides' },
   { href: '/about', title: 'About' },
 ]
 
