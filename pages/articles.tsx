a98ed15a894e5f48220f2b84e81a08a83a996e92
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-10-26 01:28:16 +0900

    Search articles

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 import articlesData from '@/data/articlesData'
 import SmallCard from '@/components/SmallCard'
 import { PageSEO } from '@/components/SEO'
 
 export default function Articles() {
+  const [searchValue, setSearchValue] = useState('')
+  const filteredArticles = articlesData.filter((frontMatter) => {
+    const searchContent = frontMatter.title + frontMatter.description
+    return searchContent.toLowerCase().includes(searchValue.toLowerCase())
+  })
   return (
     <>
       <PageSEO title={`Articles - ${siteMetadata.author}`} description={siteMetadata.description} />
@@ -12,13 +18,34 @@ export default function Articles() {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Articles
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
-          </p>
+          <div className="relative max-w-lg">
+            <input
+              aria-label="Search articles"
+              type="text"
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search articles"
+              className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+            />
+            <svg
+              className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
         </div>
         <div className="container py-12">
           <div className="flex flex-wrap -m-4">
-            {articlesData.map((d) => (
+            {!filteredArticles.length && 'No articles found.'}
+            {filteredArticles.map((d) => (
               <SmallCard
                 key={d.title}
                 title={d.title}
