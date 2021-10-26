36683fea5c3f7fd36024c507224e90819c96c179
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-10-26 17:20:25 +0900

    h2

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,9 @@ export default function Home() {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
             Articles
-          </h1>
+          </h2>
         </div>
         <div className="container py-12">
           <div className="flex flex-wrap -m-4">
@@ -48,9 +48,9 @@ export default function Home() {
       )}
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
             Slides
-          </h1>
+          </h2>
         </div>
         <div className="container py-12">
           <div className="flex flex-wrap -m-4">
@@ -80,9 +80,9 @@ export default function Home() {
       )}
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
             Projects
-          </h1>
+          </h2>
         </div>
         <div className="container py-12">
           <div className="flex flex-wrap -m-4">
