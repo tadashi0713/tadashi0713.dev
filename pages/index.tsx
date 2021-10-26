0d73ba8eeb480ebe29e2b0cb2fa5613c4ecd8457
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-10-26 17:49:53 +0900

    Add intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,21 @@ export default function Home() {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <div className="flex flex-col items-center my-6 xl:flex-row gap-x-12 xl:mb-12">
+        <div className="pt-6">
+          <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Hi, I’m Tadashi Nemoto
+          </h1>
+          <h2 className="text-lg prose text-gray-600 dark:text-gray-400">
+            {`Welcome to my blog - ${siteMetadata.description}. I am the co-founder of Cylynx, a data
+            scientist by profession and economist by training. In my free time, I like developing `}
+            <Link href="/projects">side projects</Link>
+            {' and '}
+            <Link href="/blog">blogging</Link>
+            {' about them. Have a good read!'}
+          </h2>
+        </div>
+      </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
