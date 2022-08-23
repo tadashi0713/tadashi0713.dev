d6f6183bda748471c0fa303c648fa9d727d51ac2
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-08-24 01:35:19 +0900

    Fix project data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,56 +1,49 @@
 const projectsData = [
   {
-    title:
-      'GitHub - tadashi0713/howtheytest-jp: 日本のソフトウェア企業のテスト・テスト自動化に関する資料をまとめています',
-    description: `日本のソフトウェア企業のテスト・テスト自動化に関する資料をまとめています. Contribute to tadashi0713/howtheytest-jp development by creating an account on GitHub.`,
+    title: 'GitHub - tadashi0713/howtheytest-jp',
+    description: `日本のソフトウェア企業のテスト・テスト自動化に関する資料をまとめています.`,
     imgSrc:
       'https://repository-images.githubusercontent.com/265459396/c2e04280-9b99-11ea-9ada-2270bd8dc602',
     href: 'https://github.com/tadashi0713/howtheytest-jp',
   },
   {
-    title:
-      'GitHub - tadashi0713/circleci-graviton-fargate-demo: CircleCI(Arm Executor) + AWS Graviton2 on AWS Fargate Demo',
-    description: `CircleCI(Arm Executor) + AWS Graviton2 on AWS Fargate Demo - GitHub - tadashi0713/circleci-graviton-fargate-demo: CircleCI(Arm Executor) + AWS Graviton2 on AWS Fargate Demo`,
+    title: 'GitHub - tadashi0713/circleci-graviton-fargate-demo',
+    description: `CircleCI(Arm Executor) + AWS Graviton2 on AWS Fargate Demo`,
     imgSrc:
       'https://repository-images.githubusercontent.com/492660350/8cf99b0b-713b-4c1f-bea7-83d0b46386ca',
     href: '	https://github.com/tadashi0713/circleci-graviton-fargate-demo',
   },
   {
-    title:
-      'GitHub - tadashi0713/circleci-runner-ec2-mac-terraform: Terraform / Packer template to deploy autoscaling CircleCI Runner of EC2 Mac instances.',
-    description: `Terraform / Packer template to deploy autoscaling CircleCI Runner of EC2 Mac instances. - GitHub - tadashi0713/circleci-runner-ec2-mac-terraform: Terraform / Packer template to deploy autoscaling CircleCI Runner of EC2 Mac instances.`,
+    title: 'GitHub - tadashi0713/circleci-runner-ec2-mac-terraform',
+    description: `Terraform / Packer template to deploy autoscaling CircleCI Runner of EC2 Mac instances.`,
     imgSrc:
       'https://repository-images.githubusercontent.com/459023040/6ece0f56-a9c4-4dc9-8563-0fe427eeb5b9',
     href: 'https://github.com/tadashi0713/circleci-runner-ec2-mac-terraform',
   },
   {
-    title:
-      'GitHub - CircleCI-Public/flutter-orb: Easily install Flutter SDK and package dependencies. Also wasily run unit test and lint. Supports Linux and macOS',
-    description: `Easily install Flutter SDK and package dependencies. Also wasily run unit test and lint. Supports Linux and macOS - GitHub - CircleCI-Public/flutter-orb: Easily install Flutter SDK and package dependencies. Also wasily run unit test and lint. Supports Linux and macOS`,
+    title: 'GitHub - CircleCI-Public/flutter-orb',
+    description: `Easily install Flutter SDK and package dependencies. Also wasily run unit test and lint. Supports Linux and macOS`,
     imgSrc:
       'https://opengraph.githubassets.com/1415841adafd526a9ac7cbe6363d0e43884a290ace352e42bf7a28b50f42de9f/CircleCI-Public/flutter-orb',
     href: 'https://github.com/CircleCI-Public/flutter-orb',
   },
   {
-    title:
-      'GitHub - tadashi0713/circleci_flutter_demo: Demo for CI/CD pipeline for Flutter app using CircleCI.',
-    description: `Demo for CI/CD pipeline for Flutter app using CircleCI. - GitHub - tadashi0713/circleci_flutter_demo: Demo for CI/CD pipeline for Flutter app using CircleCI.`,
+    title: 'GitHub - tadashi0713/circleci_flutter_demo',
+    description: `Demo for CI/CD pipeline for Flutter app using CircleCI.`,
     imgSrc:
       'https://opengraph.githubassets.com/f71ba98b5a9f959dfd3ef6d7380fe96ae8b4f8d19c395f99b38c0a1f9e39eb08/tadashi0713/circleci_flutter_demo',
     href: 'https://github.com/tadashi0713/circleci_flutter_demo',
   },
   {
-    title:
-      'GitHub - tadashi0713/circleci-reactnative-detox-demo: Demo for CI/CD pipeline for React Native + Detox app using CircleCI.',
-    description: `Demo for CI/CD pipeline for React Native + Detox app using CircleCI. - GitHub - tadashi0713/circleci-reactnative-detox-demo: Demo for CI/CD pipeline for React Native + Detox app using CircleCI.`,
+    title: 'GitHub - tadashi0713/circleci-reactnative-detox-demo',
+    description: `Demo for CI/CD pipeline for React Native + Detox app using CircleCI.`,
     imgSrc:
       'https://opengraph.githubassets.com/1fd587308bfd81e8accac570398fb14698cfb35ddb8d5a1cfd494b47dd37a5cc/tadashi0713/circleci-reactnative-detox-demo',
     href: 'https://github.com/tadashi0713/circleci-reactnative-detox-demo',
   },
   {
-    title:
-      'GitHub - tadashi0713/circleci-demo-ip-ranges: Demo to run CircleCI jobs(Cypress) to IP-based restricted application using IP Ranges feature.',
-    description: `Demo to run CircleCI jobs(Cypress) to IP-based restricted application using IP Ranges feature. - GitHub - tadashi0713/circleci-demo-ip-ranges: Demo to run CircleCI jobs(Cypress) to IP-based restricted application using IP Ranges feature.`,
+    title: 'GitHub - tadashi0713/circleci-demo-ip-ranges',
+    description: `Demo to run CircleCI jobs(Cypress) to IP-based restricted application using IP Ranges feature.`,
     imgSrc:
       'https://opengraph.githubassets.com/6aab29c62a9e297fdb50fd62c4c684614024c3159e800da0f5d7a7ed0849b355/tadashi0713/circleci-demo-ip-ranges',
     href: 'https://github.com/tadashi0713/circleci-demo-ip-ranges',
