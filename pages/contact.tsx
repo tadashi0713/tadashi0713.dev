f6785e2f13ce9122444d08474b5bf483da62e000
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-09-05 21:04:51 +0900

    Iframe

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,17 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 export default function Contact() {
-  return <div />
+  return (
+    <div>
+      <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" async></script>
+      <iframe
+        className="airtable-embed airtable-dynamic-height"
+        src="https://airtable.com/embed/shrJ0A9ke1QkJZ2wt?backgroundColor=blue"
+        frameBorder="0"
+        onmousewheel=""
+        width="100%"
+        height="1207"
+        // style="background: transparent; border: 1px solid #ccc;"
+      ></iframe>
+    </div>
+  )
 }
