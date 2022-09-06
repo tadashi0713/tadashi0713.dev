aa3923ce91be2c7bbfb18029611a154b4dbe2a64
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-09-06 12:27:42 +0900

    add seo

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -5,6 +5,7 @@ const headerNavLinks = [
   // { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
+  { href: '/contact', title: 'Contact' },
 ]
 
 export default headerNavLinks
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,15 +1,21 @@
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+
 /* eslint-disable jsx-a11y/iframe-has-title */
 export default function Contact() {
   return (
-    <div className="flex flex-col items-start justify-start md:justify-center md:items-center md:flex-row md:space-x-6 md:mt-24">
-      <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" async></script>
-      <iframe
-        className="airtable-embed airtable-dynamic-height"
-        src="https://airtable.com/embed/shrJ0A9ke1QkJZ2wt?backgroundColor=blue"
-        frameBorder="0"
-        width="100%"
-        height="1207"
-      ></iframe>
-    </div>
+    <>
+      <PageSEO title={`Contact - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <div className="flex flex-col items-start justify-start md:justify-center md:items-center md:flex-row md:space-x-6 md:mt-24">
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" async></script>
+        <iframe
+          className="airtable-embed airtable-dynamic-height"
+          src="https://airtable.com/embed/shrJ0A9ke1QkJZ2wt?backgroundColor=blue"
+          frameBorder="0"
+          width="100%"
+          height="1207"
+        ></iframe>
+      </div>
+    </>
   )
 }
