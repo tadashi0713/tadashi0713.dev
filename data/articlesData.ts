0ae5242fa3622170d2b55a1b4d560b9cea1f535d
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-12-21 15:26:07 +0900

    Android espresso

--- data/articlesData.ts
+++ data/articlesData.ts
@@ -1,5 +1,12 @@
 const articlesData = [
   {
+    title: 'Androidエミュレーターを使ったUIテスト(Espresso)を分割・並列実行しよう',
+    description: `この記事では、Androidエミュレーターを使ったUIテスト(Espresso)を分割・並列実行することによって、実行時間を短縮する方法について紹介します。`,
+    imgSrc:
+      'https://res.cloudinary.com/tadashi0713/image/upload/v1671603912/tadashi0713.dev/android-parallel-testing-hero.png',
+    href: 'https://circleci.com/ja/blog/unity-mobile-cicd',
+  },
+  {
     title: 'Unity を使ったモバイルゲーム CI/CD 環境をクラウドネイティブ化しよう(GameCI)',
     description: `この記事では、GameCI と CircleCI を利用して、Unity を使ったモバイルゲーム CI/CD 環境をクラウドネイティブ化する方法について紹介します。`,
     imgSrc:
