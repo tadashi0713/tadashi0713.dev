34e866d37ebc2d6c5f84f56d8a6b9dfeba63eae5
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-11-21 11:57:51 +0900

    Add resume

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,7 +4,8 @@ const headerNavLinks = [
   { href: '/projects', title: 'Projects' },
   // { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
+  // { href: '/about', title: 'About' },
+  { href: 'https://resume.io/r/DdFtcnNf8', title: 'Resume' },
   { href: '/contact', title: 'Contact' },
 ]
 
