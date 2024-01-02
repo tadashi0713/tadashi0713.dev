5578cb190cac651d1a3ae60c49bfd6fa51b0f68a
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2024-01-02 14:53:49 +0900

    tag

--- components/Link.tsx
+++ components/Link.tsx
@@ -7,12 +7,8 @@ const CustomLink = ({ href, ...rest }: LinkProps & AnchorHTMLAttributes<HTMLAnch
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
 
-  if (isInternalLink) {
+  if (isInternalLink || isAnchorLink) {
     return <Link href={href} {...rest} />
-  }
-
-  if (isAnchorLink) {
-    return <a href={href} {...rest} />
   }
 
   return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
