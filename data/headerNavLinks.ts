5a6a94cf27b8660295faf9f21203b74c04a192d1
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-09-30 10:00:09 +0900

    Add documents

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -5,6 +5,7 @@ const headerNavLinks = [
   // { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   // { href: '/about', title: 'About' },
+  { href: 'https://docs.tadashi0713.dev', title: 'Documents' },
   { href: 'https://resume.io/r/DdFtcnNf8', title: 'Resume' },
   { href: '/contact', title: 'Contact' },
 ]
