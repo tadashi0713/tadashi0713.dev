58d140cc8dba9a7a385c6bc4bd8f879d44a04d5c
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-04-08 11:53:38 +0900

    add to data

--- data/featuredItemsData.ts
+++ data/featuredItemsData.ts
@@ -1,5 +1,12 @@
 const featuredItemsData = [
   {
+    title: `Solve Docker Hub's Pull Rate Limit on CI/CD Pipelines using Organization Access Tokens`,
+    description:
+      'This article explains how to solve Docker Hub’s Pull Rate Limit on CI/CD pipelines using Organization Access Tokens.',
+    imgSrc: '/static/images/dockerhub-oats-ci.png',
+    href: '/blog/en/dockerhub-oats-ci',
+  },
+  {
     title: 'Docker Scout を使ったコンテナセキュリティの可視化と管理',
     description:
       'Docker Scoutは、コンテナのセキュリティ管理を効率化し、ソフトウェアサプライチェーン全体の安全性を確保するための最新ツールです。本ウェビナーでは、Docker Scoutの最新機能や脆弱性の特定や対応方法、運用のベストプラクティスについて詳しく解説します。',
@@ -12,13 +19,7 @@ const featuredItemsData = [
     description: `Learn how integrating Docker Build Cloud into a CI/CD pipeline using AWS CodeBuild can dramatically reduce build times and improve release frequency.`,
     imgSrc: '/static/images/2400x1260_generic-dbc-blog_e.png',
     href: 'https://www.docker.com/blog/accelerate-your-docker-builds-using-aws-codebuild-and-docker-build-cloud/',
-  },
-  {
-    title: 'Testcontainers ガイド',
-    description: `以下のガイドでは、実践的なプロジェクトを通じて Testcontainers を学ぶことができます。初心者から上級者まで、どのレベルの方にも役立つガイドが用意されているはずです。スキルをゼロから習得したい方も、すでに専門的な知識をお持ちの方も、ぜひお試しください。`,
-    imgSrc: '/static/images/testcontainers-guides-ja.png',
-    href: 'https://docs.tadashi0713.dev/testcontainers-guides-ja',
-  },
+  }
 ]
 
 export default featuredItemsData
