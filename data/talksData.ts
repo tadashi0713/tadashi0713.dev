8e93fccaffcdea47f1499621339ae3e934b0acdc
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-04-18 12:55:17 +0900

    docker model runner

--- data/talksData.ts
+++ data/talksData.ts
@@ -1,11 +1,19 @@
 const talksData = [
   {
+    title: 'Docker Model Runner 〜ローカル環境で LLM を簡単に実行、Docker が提案する新しい生成AIアプリ開発体験〜',
+    description:
+      'Docker Model Runner は 大規模言語モデル(LLM) をDocker を使ったコンテナ開発と同じ体験でローカル環境で実行することができる、Docker Desktop の新たな機能です。',
+    imgSrc:
+      '/static/images/docker-model-runner.jpg',
+    href: 'https://www.youtube.com/watch?v=b9dH0Q_BKcA',
+  },
+  {
     title: 'Docker Scout を使ったコンテナセキュリティの可視化と管理',
     description:
       'Docker Scoutは、コンテナのセキュリティ管理を効率化し、ソフトウェアサプライチェーン全体の安全性を確保するための最新ツールです。本ウェビナーでは、Docker Scoutの最新機能や脆弱性の特定や対応方法、運用のベストプラクティスについて詳しく解説します。',
     imgSrc:
       '/static/images/docker-scout-webinar-ja.jpeg',
-    href: 'https://www.docswell.com/s/tadashi0713/ZN18EQ-docker-scout-webinar-ja',
+    href: 'https://www.youtube.com/watch?v=wuV8hO3Tm_0',
   },
   {
     title: 'CircleCI と Argo Rollouts を統合した GKE への継続的リリース基盤',
