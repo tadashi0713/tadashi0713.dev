b65ca31bec712403e037e1a986ea5e70ddbb982e
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-04-30 14:09:14 +0900

    photos

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,10 +2,8 @@ const headerNavLinks = [
   { href: '/articles', title: 'Articles' },
   { href: '/talks', title: 'Talks' },
   { href: '/projects', title: 'Projects' },
-  // { href: '/blog', title: 'Blog' },
-  // { href: '/tags', title: 'Tags' },
-  // { href: '/about', title: 'About' },
   { href: 'https://docs.tadashi0713.dev', title: 'Documents' },
+  { href: 'https://photos.tadashi0713.dev', title: 'Photos' },
   { href: 'https://resume.io/r/DdFtcnNf8', title: 'Resume' },
   { href: '/contact', title: 'Contact' },
 ]
