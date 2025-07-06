219bd34eab9ad17f756efebf17e653509ef1833b
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-07-06 18:57:38 +0900

    disable search

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,6 @@ import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import SearchButton from './SearchButton'
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
@@ -42,7 +41,6 @@ const Header = () => {
               </Link>
             ))}
         </div>
-        <SearchButton />
         <ThemeSwitch />
         <MobileNav />
       </div>
