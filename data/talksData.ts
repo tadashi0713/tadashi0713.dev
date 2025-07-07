8f8ad524be4353badff7973561d64658717f2cbf
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-07-07 15:10:49 +0900

    until devops guide

--- data/talksData.ts
+++ data/talksData.ts
@@ -41,8 +41,7 @@ const talksData: Talk[] = [
     title: 'CircleCI と Argo Rollouts を統合した GKE への継続的リリース基盤',
     description:
       'GKEをはじめとしたKubernetesへのCI/CDには様々なアプローチがありますが、多くの場合CIとCDツールが別々になり管理や可視化が難しくなります。本セッションではCircleCIとArgo Rolloutsを連携し、CI(ビルド・テスト)からCD(デプロイ・段階リリース)までを統合されたワークフローにすることによって、安全かつ効率的なアプリケーション開発を実現する方法をご紹介します。',
-    imgSrc:
-      'https://res.cloudinary.com/tadashi0713/image/upload/v1722416890/tadashi0713.dev/google-cloud-next-2024.png',
+    imgSrc: '/static/images/google-cloud-next-2024.png',
     href: 'https://www.docswell.com/s/tadashi0713/KNREWL-google-cloud-next-2024',
   },
   {
@@ -83,8 +82,7 @@ const talksData: Talk[] = [
     title: '自動テストをCIで”実行”させるだけでは不十分な理由',
     description:
       'CircleCI＆Autify共催セミナー「CI/CD、テスト自動化で実現する高品質・高速なプロダクト開発」',
-    imgSrc:
-      'https://res.cloudinary.com/tadashi0713/image/upload/v1690864679/tadashi0713.dev/19a26443fe31c577ccbcf9a99bbef98c0c561c2d.png',
+    imgSrc: '/static/images/autify-webinar.png',
     href: 'https://www.docswell.com/s/tadashi0713/KP9G32-circleci-autify-webinar',
   },
   {
